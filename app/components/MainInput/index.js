48ed14436e4aea773558383c4c8863825d81a8bf
Alexandr Subbotin <kelionweb@gmail.com>
2016-06-22 23:09:44 +0200

    Fix autofocus

--- app/components/MainInput/index.js
+++ app/components/MainInput/index.js
@@ -2,12 +2,15 @@ import React, { Component, PropTypes } from 'react';
 import styles from './styles.css';
 
 export default class MainInput extends Component {
+  componentDidMount(){
+     this.refs.input.focus();
+  }
   render() {
     return (
       <input
-        placeholder="Search..."
-        type="text"
-        autofocus="true"
+        placeholder='Search...'
+        type='text'
+        ref='input'
         value = {this.props.value}
         className={styles.input}
         onKeyDown={this.props.onKeyDown}

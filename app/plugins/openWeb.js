715d5c08f75b66dfddcf9d55729066c0999b2a00
Alexandr Subbotin <kelionweb@gmail.com>
2016-07-02 11:39:01 +0200

    Added cache for opened urls

--- app/plugins/openWeb.js
+++ app/plugins/openWeb.js
@@ -1,22 +1,44 @@
 import shellCommand from '../lib/shellCommand';
 
-const URL_REGEXP = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
+const URL_REGEXP = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
 
-const openWebPlugin = function(term, callback) {
+// Cache used urls to autocomplete them
+let lastUrls = [];
+
+function uniq(arr) {
+  return arr.filter((elem, pos, source) => source.indexOf(elem) === pos);
+}
+
+function matchedUsedUrls(term) {
+  return lastUrls.filter(url => url.replace(/^https?:\/\//, '').indexOf(term) === 0);
+}
+
+function toResult(url) {
+  return {
+    title: url,
+    id: `url-${url}`,
+    subtitle: 'Open url',
+    term: url,
+    onSelect: () => {
+      lastUrls = uniq([url, ...lastUrls]);
+      shellCommand(`open ${url}`);
+    },
+  };
+}
+
+const openWebPlugin = (term, callback) => {
   const match = term.match(URL_REGEXP);
   if (match) {
     let url = term;
-    if (!term.match(/^https?\/\//)) {
+    if (!term.match(/^https?:\/\//)) {
       url = `http://${url}`;
     }
-    callback(term, {
-      title: url,
-      subtitle: 'Open url',
-      onSelect: () => { shellCommand(`open ${url}`) },
-    });
+    callback(term, toResult(url));
   }
-}
+  const result = matchedUsedUrls(term).map(toResult);
+  callback(term, result);
+};
 
 export default {
   fn: openWebPlugin,
-}
+};

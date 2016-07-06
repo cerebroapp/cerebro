f1e9d1d639ba8c8da78e44b60414b8f28ed16b57
Alexandr Subbotin <kelionweb@gmail.com>
2016-07-06 22:18:40 +0200

    Removed unused routing reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { routerReducer as routing } from 'react-router-redux';
 import search from './search';
 import icons from './icons';
 
 const rootReducer = combineReducers({
-  routing,
   search,
   icons,
 });

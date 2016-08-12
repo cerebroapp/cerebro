0495895d683141fcbbb343bb577ffca0cf8a4675
Alexandr Subbotin <kelionweb@gmail.com>
2016-08-12 18:50:39 +0200

    Prettify archive & mardown previews

--- main/plugins/files/getPreview/preview/Archive.js
+++ main/plugins/files/getPreview/preview/Archive.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import listArchive from 'lib/listArchive';
-import FileDetails from '../../../../components/FileDetails';
+import NativeIcon from 'main/components/NativeIcon';
+import FileDetails from 'main/components/FileDetails';
 import Loading from 'main/components/Loading';
 import styles from './styles.css'
 
@@ -30,8 +31,15 @@ export default class Archive extends Component {
     const { path } = this.props;
     if (this.state.error) return <div>Error fetching archive</div>;
     if (!this.state.loaded) return <Loading />;
+    const type = this.props.path.match(/\.(\w+)$/)[1];
+    const icon = `/System/Library/CoreServices/Applications/Archive Utility.app/Contents/Resources/bah-${type}.icns`;
+    console.log(icon);
     return (
       <div className={styles.previewArchive}>
+        <div className={styles.previewIcon}>
+          <NativeIcon path={icon} />
+        </div>
+        <div className={styles.filesListText}>Files:</div>
         <ul key={path}>{this.renderList()}</ul>
         <FileDetails path={path} />
       </div>)

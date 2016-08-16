11a02dbf497e301aecf72114d08f5be0e04865f3
Alexandr Subbotin <kelionweb@gmail.com>
2016-08-16 22:00:41 +0200

    Preload some generic icons

--- background/initializers/preloadIcons.js
+++ background/initializers/preloadIcons.js
@@ -1,6 +1,21 @@
 import { loadIcon } from '../../main/actions/icons';
 import getAppsList from 'lib/getAppsList.js';
 
+// List of generic icons for preload
+const GENERIC_ICONS = [
+  '/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/GenericFolderIcon.icns',
+  '/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/ExecutableBinaryIcon.icns',
+  '/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/GenericApplicationIcon.icns',
+];
+
+const ARCHIVE_ICONS = [
+  '/System/Library/CoreServices/Applications/Archive\ Utility.app/Contents/Resources/bah-zip.icns',
+  '/System/Library/CoreServices/Applications/Archive\ Utility.app/Contents/Resources/bah-tar.icns',
+  '/System/Library/CoreServices/Applications/Archive\ Utility.app/Contents/Resources/bah-tgz.icns',
+  '/System/Library/CoreServices/Applications/Archive\ Utility.app/Contents/Resources/bah-gz.icns',
+  '/System/Library/CoreServices/Applications/Archive\ Utility.app/Contents/Resources/bah.icns'
+];
+
 export default (send) => {
   const dispatch = (action) => {
     send({
@@ -9,7 +24,11 @@ export default (send) => {
     });
   }
 
+  // Preload all application icons
   getAppsList().then(items => {
-    items.forEach(app => loadIcon(app.path)(dispatch))
-  })
+    items
+      .concat(GENERIC_ICONS)
+      .concat(ARCHIVE_ICONS)
+      .forEach(app => loadIcon(app.path)(dispatch))
+  });
 }

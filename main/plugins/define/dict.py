7b4ab0310e46ecfa287766a5846a8036641cbfc9
Alexandr Subbotin <kelionweb@gmail.com>
2016-08-30 17:52:41 +0200

    Speed up define plugin
    Speed up by importing only main DictionaryServices instead of `from DictionaryServices import *`

--- main/plugins/define/dict.py
+++ main/plugins/define/dict.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from DictionaryServices import *
+import DictionaryServices
 
 def main():
     searchword = sys.argv[1].decode('utf-8')
     wordrange = (0, len(searchword))
-    dictresult = DCSCopyTextDefinition(None, searchword, wordrange)
+    dictresult = DictionaryServices.DCSCopyTextDefinition(None, searchword, wordrange)
     print dictresult.encode('utf-8')
 
 if __name__ == '__main__':

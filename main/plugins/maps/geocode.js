a7b586e132b3e7edab128a60709151e2b1646259
Alexandr Subbotin <kelionweb@gmail.com>
2016-09-11 00:49:56 +0200

    Use locale language for google maps geocode

--- main/plugins/maps/geocode.js
+++ main/plugins/maps/geocode.js
@@ -1,7 +1,7 @@
-const LANG = 'ru';
+import { lang } from 'lib/locale';
 
 export default (term) => {
-  const url = `http://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(term)}&language=${LANG}`;
+  const url = `http://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(term)}&language=${lang}`;
   return fetch(url)
     .then(response => response.json())
     .then(json => json.results);

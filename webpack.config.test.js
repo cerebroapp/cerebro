61640aa613a7689b9ba3fd55bb6b39562ec49553
Alexandr Subbotin <kelionweb@gmail.com>
2016-11-16 13:30:40 +0100

    added null-loader that is used for css in tests

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -3,21 +3,12 @@ const config = require('./webpack.config.base');
 config.target = 'electron-renderer'
 
 config.module = Object.assign(config.module,{
+  debug: true,
+  devtool: 'cheap-module-eval-source-map',
   loaders: Array.prototype.concat.call(config.module.loaders, [
     {
-      test: /\.global\.css$/,
-      loaders: [
-        'style-loader',
-        'css-loader?sourceMap'
-      ]
-    },
-
-    {
-      test: /^((?!\.global).)*\.css$/,
-      loaders: [
-        'style-loader',
-        'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
-      ]
+      test: /\.css$/,
+      loader: 'null-loader'
     }
   ])
 });

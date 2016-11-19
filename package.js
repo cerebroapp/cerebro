57975c958dff24c4f9c7205c879f7548023e5c3d
Alexandr Subbotin <kelionweb@gmail.com>
2016-11-19 21:45:06 +0100

    Don't include unnecessary files in final build

--- package.js
+++ package.js
@@ -43,6 +43,12 @@ const DEFAULT_OPTS = {
     '^/main/(?!app.html)',
     '^/background/(?!index.html)',
     '^/.babelrc',
+    '^/.DS_Store',
+    '^/.editorconfig',
+    '^/.eslint.*',
+    '^/.git.*',
+    '^/.travis.yml',
+    '^/.tmp',
   ]
   .concat(devDeps.map(name => `/node_modules/${name}($|/)`))
   .concat(

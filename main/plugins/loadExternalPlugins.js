98de874e38c578a26b2effef255916d2febd3923
Alexandr Subbotin <kelionweb@gmail.com>
2016-11-30 10:11:41 +0100

    Catch plugin loading errors

--- main/plugins/loadExternalPlugins.js
+++ main/plugins/loadExternalPlugins.js
@@ -5,20 +5,43 @@ import path from 'path';
 const electronApp = remote ? remote.app : app;
 const externalPluginsPath = electronApp.getPath('userData') + '/plugins';
 
+/**
+ * Validate plugin module
+ *
+ * @param  {[type]} plugin [description]
+ * @return {[type]}        [description]
+ */
+const validatePlugin = (plugin) => (
+  plugin &&
+    // Check existing of main plugin function
+    typeof plugin.fn === 'function' &&
+    // Check that plugin function accepts 0 or 1 argument
+    plugin.fn.length <= 1
+);
+
 export default () => {
   if (!fs.existsSync(externalPluginsPath)) {
     fs.mkdirSync(externalPluginsPath);
   }
   const files = fs.readdirSync(externalPluginsPath);
-  const plugins = files.reduce((acc, file) => {
+  return files.reduce((acc, file) => {
     const pluginPath = path.join(externalPluginsPath, file);
     const isDir = fs.statSync(pluginPath).isDirectory();
     if (isDir) {
-      console.log('Load external plugin...', pluginPath);
-      // Use window.require instead of webpack require because
-      acc[file] = window.require(pluginPath);
+      try {
+        console.log('Loading external plugin ', pluginPath);
+        const plugin = window.require(pluginPath);
+        if (!validatePlugin(plugin)) {
+          console.log('External plugin is not valid');
+          return;
+        }
+        acc[file] = plugin;
+      } catch(error) {
+        // catch all errors from plugin loading
+        console.log('Error loading external plugin');
+        console.log(error);
+      }
     }
     return acc;
   }, {});
-  return plugins;
 }

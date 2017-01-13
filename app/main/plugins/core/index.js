f9cdc0de960c85302ad204043439c746a158c49c
Stafford Williams <stafford.williams@gmail.com>
2017-01-13 18:42:26 +1100

    exclude dictionary from windows

--- app/main/plugins/core/index.js
+++ app/main/plugins/core/index.js
@@ -3,7 +3,7 @@ export { default as apps } from './apps'
 export { default as autocomplete } from './autocomplete'
 export let contacts;
 export { default as converter } from './converter'
-export { default as define } from './define'
+export let define;
 export { default as files } from './files'
 export { default as google } from './google'
 export { default as maps } from './maps'
@@ -14,5 +14,7 @@ export { default as settings } from './settings'
 export { default as system } from './system'
 export { default as translate } from './translate'
 
-if (process.platform === "darwin")
+if (process.platform === "darwin") {
     contacts = require('./contacts');
+    define = require('./define')
+}

2bb728fd8099be76ab16ffd9057532882b20e086
Alexandr Subbotin <kelionweb@gmail.com>
2017-01-17 01:40:47 +0100

    Added rmdir and targz packages
    So now it should work on windows

--- app/lib/plugins/npm.js
+++ app/lib/plugins/npm.js
@@ -1,5 +1,26 @@
-import { shellCommand } from 'cerebro-tools'
 import fs from 'fs'
+import rmdir from 'rmdir'
+import path from 'path'
+import targz from 'tar.gz'
+
+/**
+ * Promise-wrapper for rmdir
+ * @param  {String} dir
+ * @return {Promise}
+ */
+const removeDir = (dir) => new Promise((resolve, reject) => {
+  rmdir(dir, err => err ? reject() : resolve())
+})
+
+/**
+ * Promise-wrapper for tar.Extract
+ * @param  {String} tarPath
+ * @param  {String} dest
+ * @return {Promise}
+ */
+const untar = (tarPath, dest) => new Promise((resolve, reject) => {
+  targz().extract(tarPath, dest, err => err ? reject() : resolve())
+})
 
 /**
  * Base url of npm API
@@ -14,8 +35,8 @@ const API_BASE = 'http://registry.npmjs.org/'
  * @param  {String} path Url for package .tgz file
  * @return {Promise<Buffer>}
  */
-const fetchTarball = (path) => (
-  fetch(path)
+const fetchTarball = (tarBall) => (
+  fetch(tarBall)
     .then(response => response.arrayBuffer())
     .then(content => new Buffer(content))
 )
@@ -29,12 +50,12 @@ const fetchTarball = (path) => (
  */
 const unarchiveTarball = (tarPath, packagePath) => {
   const prepare = fs.existsSync(packagePath) ?
-    shellCommand(`rm -rf "${packagePath}"`) :
+    removeDir(packagePath) :
     Promise.resolve()
   return prepare.then(() => {
     fs.mkdirSync(packagePath)
-    return shellCommand(`tar zxvf "${tarPath}" -C "${packagePath}"`)
-      .then(() => shellCommand(`rm -rf "${tarPath}"`))
+    return untar(tarPath, packagePath)
+      .then(() => removeDir(tarPath))
       .then(() => packagePath)
   })
 }
@@ -46,8 +67,8 @@ const unarchiveTarball = (tarPath, packagePath) => {
  * @param  {String} path Path to npm package directory
  * @return {Object}
  */
-export default (path) => {
-  const packageJson = `${path}/package.json`
+export default (dir) => {
+  const packageJson = path.join(dir, 'package.json')
   const setConfig = (config) => (
     fs.writeFileSync(packageJson, JSON.stringify(config))
   )
@@ -70,14 +91,14 @@ export default (path) => {
           return fetchTarball(tar)
         })
         .then(body => {
-          const tarPath = `${path}/${name}.tgz`
-          const tmpPath = `${path}/${name}-tmp`
+          const tarPath = path.join(dir, `${name}.tgz`)
+          const tmpPath = path.join(dir, `${name}-tmp`)
           fs.writeFileSync(tarPath, body)
           return unarchiveTarball(tarPath, tmpPath)
         })
         .then((tmpPath) => {
-          fs.renameSync(`${tmpPath}/package`, `${path}/node_modules/${name}`)
-          return shellCommand(`rm -rf "${tmpPath}"`)
+          fs.renameSync(path.join(tmpPath, 'package'), path.join(dir, 'node_modules', name))
+          return removeDir(tmpPath)
         })
         .then(() => {
           const json = getConfig()
@@ -92,7 +113,8 @@ export default (path) => {
      * @return {Promise}
      */
     uninstall(name) {
-      return shellCommand(`rm -rf "${path}/node_modules/${name}"`)
+      const modulePath = path.join(dir, 'node_modules', name)
+      return removeDir(modulePath)
         .then(() => {
           const json = getConfig()
           json.dependencies = Object

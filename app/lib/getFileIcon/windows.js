c91791aaf714a7712088b23b637a3096848ec703
Jan Zípek <jan@zipek.cz>
2017-03-01 22:35:17 +0100

    Fixed codestyle.

--- app/lib/getFileIcon/windows.js
+++ app/lib/getFileIcon/windows.js
@@ -4,21 +4,8 @@ import extractIcon from 'win-icon-extractor'
  * Get system icon for file
  *
  * @param  {String} path File path
- * @param  {Number} options.width
- * @param  {[type]} options.height
  * @return {Promise<String>} Promise resolves base64-encoded source of icon
  */
-export default function getFileIcon(path, { width = 128, height = 128 } = {}) {
-  return new Promise((resolve, reject) => {
-    var icon = null;
-    try {
-      icon = extractIcon(path)
-    } catch (e) {
-      reject(e)
-    }
-
-    if (icon != null) {
-      resolve(icon)
-    }
-  })
+export default function getFileIcon(path) {
+  return extractIcon(path)
 }

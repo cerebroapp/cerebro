4c96a1c7f70a7b52e650585f622baac698d9ae3c
Jan Zípek <jan@zipek.cz>
2017-03-01 21:46:35 +0100

    Pass icon path for windows/linux to frontend.

--- app/main/plugins/core/basic-apps/index.js
+++ app/main/plugins/core/basic-apps/index.js
@@ -14,7 +14,7 @@ const fn = ({ term, actions, display }) => {
   const result = search(appsList, term, toString).map(app => {
     const { id, path, name, description, icon } = app
     return {
-      icon,
+      icon: path,
       id: id || path,
       title: name,
       term: name,

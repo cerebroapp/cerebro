d1f526ffe63a3b8132730e39d7061515c096ef12
Jan Zípek <jan@zipek.cz>
2017-04-10 17:20:04 +0200

    Switch to SmartIcon in preview

--- app/main/plugins/core/basic-apps/Preview.js
+++ app/main/plugins/core/basic-apps/Preview.js
@@ -1,12 +1,13 @@
 import React, { PropTypes } from 'react'
 import FileDetails from 'main/components/FileDetails'
 import styles from './styles.css'
+import SmartIcon from 'main/components/SmartIcon'
 
 const Preview = ({ path, name, icon }) => (
   <div>
     {icon &&
       <div className={styles.previewIcon}>
-        <img src={icon} alt="" />
+        <SmartIcon path={icon} />
       </div>
     }
     <div className={styles.previewName}>{name}</div>

5f3ecf9a9b53ccf11d0b453ba72d68a84b1768dc
Alexandr Subbotin <kelionweb@gmail.com>
2017-05-17 13:47:21 +0200

    Sort plugins by popularity

--- app/main/plugins/core/cerebro/plugins/getAvailablePlugins.js
+++ app/main/plugins/core/cerebro/plugins/getAvailablePlugins.js
@@ -1,3 +1,4 @@
+import { flow, map, sortBy } from 'lodash/fp'
 /**
  * API endpoint to search all cerebro plugins
  * @type {String}
@@ -11,11 +12,15 @@ const URL = 'https://registry.npmjs.com/-/v1/search?from=0&size=500&text=keyword
 export default () => (
   fetch(URL)
     .then(response => response.json())
-    .then(json => json.objects.map(p => ({
-      name: p.package.name,
-      version: p.package.version,
-      description: p.package.description,
-      homepage: p.package.links.homepage,
-      repo: p.package.links.repository
-    })))
+    .then(json => flow(
+        sortBy(p => -p.score.detail.popularity),
+        map(p => ({
+          name: p.package.name,
+          version: p.package.version,
+          description: p.package.description,
+          homepage: p.package.links.homepage,
+          repo: p.package.links.repository
+        }))
+      )(json.objects)
+    )
 )

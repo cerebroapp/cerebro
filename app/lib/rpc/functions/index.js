2cfc294c5392f28b290f5b46ef508dba2e063c7c
Alexandr Subbotin <kelionweb@gmail.com>
2017-05-24 17:48:27 +0200

    fix linter

--- app/lib/rpc/functions/index.js
+++ app/lib/rpc/functions/index.js
@@ -1,12 +1,2 @@
-import wrap from '../wrap'
-import { memoize } from 'cerebro-tools'
-
-const MEMOIZE_OPTIONS = {
-  length: false,
-  promise: 'then',
-  maxAge: 30 * 1000, // Default cache expires in 30 seconds,
-  preFetch: true
-}
-
 // Export RPC-versions of lib functions
 export { default as initializePlugins } from './initializePlugins'

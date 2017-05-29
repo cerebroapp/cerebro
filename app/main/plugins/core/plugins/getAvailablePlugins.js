7bb2c6ecc024fd288a919acb4f85ad606db5c9cd
Alexandr Subbotin <kelionweb@gmail.com>
2017-05-29 15:00:09 +0200

    Return back to production api endpoint for searching plugins

--- app/main/plugins/core/plugins/getAvailablePlugins.js
+++ app/main/plugins/core/plugins/getAvailablePlugins.js
@@ -4,9 +4,7 @@ import { flow, map, sortBy } from 'lodash/fp'
  * API endpoint to search all cerebro plugins
  * @type {String}
  */
-// TODO: after ending beta-testing of new version – change back to registry.npmjs.com
-// const URL = 'https://registry.npmjs.com/-/v1/search?from=0&size=500&text=keywords:cerebro-plugin'
-const URL = 'https://api.npms.io/v2/search?from=0&size=250&q=keywords%3Acerebro-plugin,cerebro-extracted-plugin'
+const URL = 'https://registry.npmjs.com/-/v1/search?from=0&size=500&text=keywords:cerebro-plugin,cerebro-extracted-plugin'
 
 /**
  * Get all available plugins for Cerebro
@@ -24,6 +22,6 @@ export default () => (
           homepage: p.package.links.homepage,
           repo: p.package.links.repository
         }))
-      )(json.results)
+      )(json.objects)
     )
 )

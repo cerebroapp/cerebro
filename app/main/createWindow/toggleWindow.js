9856ae596de05b402ecb3f42956a926b83f31b63
Alexandr Subbotin <kelionweb@gmail.com>
2017-06-08 12:57:01 +0200

    Fix no method error for `app.hide()` on Linux and Windows

--- app/main/createWindow/toggleWindow.js
+++ app/main/createWindow/toggleWindow.js
@@ -7,7 +7,11 @@ import { app } from 'electron'
 export default (appWindow) => {
   if (appWindow.isVisible()) {
     appWindow.hide()
-    app.hide()
+    if (process.platform === 'darwin') {
+      // Restore focus in previous application
+      // MacOS only: https://github.com/electron/electron/blob/master/docs/api/app.md#apphide-macos
+      app.hide()
+    }
   } else {
     appWindow.show()
     appWindow.focus()

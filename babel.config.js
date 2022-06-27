63edcd8689e0534e5a4f4213ba674ce5f1f309b2
dubisdev <dubisdev@gmail.com>
2022-06-27 19:58:26 +0200

    docs: target info

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,9 @@ module.exports = {
   presets: [
     [
       '@babel/preset-env', {
+        /** Targets must match the versions supported by electron.
+         * See https://www.electronjs.org/
+        */
         targets: {
           node: '16',
           chrome: '102'

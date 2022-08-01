cd481e01dbd69e9a76aab1b95c4b5cbd79831e71
dubisdev <dubisdev@gmail.com>
2022-08-01 18:47:19 +0200

    test: adding more unit tests

---
+++ app/lib/loadThemes.spec.js
@@ -0,0 +1,32 @@
+import themesLoader from './loadThemes'
+
+const productionThemes = [
+  {
+    value: '../dist/main/css/themes/light.css',
+    label: 'Light'
+  },
+  {
+    value: '../dist/main/css/themes/dark.css',
+    label: 'Dark'
+  }
+]
+
+const developmentThemes = [
+  {
+    value: 'http://localhost:3000/dist/main/css/themes/light.css',
+    label: 'Light'
+  },
+  {
+    value: 'http://localhost:3000/dist/main/css/themes/dark.css',
+    label: 'Dark'
+  }
+]
+
+test('returns themes for production', () => {
+  expect(themesLoader()).toEqual(productionThemes)
+})
+
+test('returns themes for development', () => {
+  process.env.HOT = true
+  expect(themesLoader()).toEqual(developmentThemes)
+})
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  collectCoverage: true,
   moduleDirectories: ['node_modules', 'app'],
   moduleNameMapper: {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',

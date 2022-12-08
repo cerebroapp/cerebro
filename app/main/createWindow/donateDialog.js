9f03b22578d2a5cf3453d1a002f1939c6b99abc3
dubisdev <dubisdev@gmail.com>
2022-12-08 17:38:40 +0100

    chore: update donate dialog function

--- app/main/createWindow/donateDialog.js
+++ app/main/createWindow/donateDialog.js
@@ -1,55 +1,50 @@
 import { dialog, shell } from 'electron'
-import config from '../../lib/config'
+import config from 'lib/config'
 
 const now = () => new Date().getTime()
 const twoWeeksAgo = () => now() - 1000 * 3600 * 24 * 7
 
 export const donate = () => {
   config.set('skipDonateDialog', true)
-  shell.openExternal('https://cerebroapp.com/#donate')
+  shell.openExternal('https://github.com/cerebroapp/cerebro#donate')
 }
 
 export const shouldShow = () => {
+  // Do not show donate dialog on first start or when "dont show this message" were chosen
   if (config.get('firstStart') || config.get('skipDonateDialog')) {
-    // Do not show donate dialog on first start or when "dont show this message" were chosen
     return false
   }
-  const lastShow = config.get('lastShownDonateDialog')
+
   // Show on second start and once per week after first start
+  const lastShow = config.get('lastShownDonateDialog')
   return !lastShow || twoWeeksAgo() >= lastShow
 }
 
-const messages = [
-  'Developers try to make you happy with this free and open-source app. Make them happy too with your donation!'
-]
-
-const buttons = [['Close', 'Make them happy']]
-const skipMessages = ["I don't want to see this message again"]
-
 export const show = () => {
   config.set('lastShownDonateDialog', now())
-  const AB = Math.floor(Math.random() * buttons.length)
 
   const options = {
-    type: 'info',
-    buttons: buttons[AB],
-    defaultId: 1,
-    cancelId: 0,
     title: 'Support Cerebro development',
-    message: messages[AB],
-    checkboxLabel: skipMessages[AB]
+    message: 'Developers try to make you happy with this free and open-source app. Make them happy too with your donation!',
+    type: 'info',
+    buttons: ['Close', 'Make them happy'],
+    cancelId: 0,
+    defaultId: 1,
+    checkboxLabel: "Don't show this message again"
   }
 
+  /**
+   * The function is called when user clicks in one of the buttons
+   * @param {number} id
+   * @param {boolean} checkboxChecked
+   */
   const callback = (id, checkboxChecked) => {
-    if (checkboxChecked) {
-      config.set('skipDonateDialog', true)
-    }
-    if (id === 1) {
-      donate()
-    }
+    if (checkboxChecked) config.set('skipDonateDialog', true)
+    if (id === 1) donate()
   }
 
   setTimeout(() => {
-    dialog.showMessageBox(options, callback)
+    dialog.showMessageBox(options)
+      .then(({ checkboxChecked, response }) => callback(response, checkboxChecked))
   }, 1000)
 }
